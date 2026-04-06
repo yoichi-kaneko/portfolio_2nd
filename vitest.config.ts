b0ad9ad22878d4979cf83a9510f6ab1a80ba4dd9
yoichi-kaneko <y.kaneko@3s.care>
2026-04-06 16:48:34 +0900

    buildエラーの修正

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,7 +6,6 @@ export default defineConfig({
   plugins: [react()],
   test: {
     environment: "jsdom",
-    environmentMatchGlobs: [["src/app/api/**/*.test.ts", "node"]],
     globals: true,
     setupFiles: ["./vitest.setup.ts"],
     exclude: ["e2e/**", "node_modules/**"],
